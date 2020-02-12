89092e6ce1a22e216f2bf8d9f9fd28e0efa61ac7
Jeremy Musser <jemusser@iu.edu>
2020-02-12 17:15:19 -0500

    Added modify endpoint for REST

--- rest_api.py
+++ rest_api.py
@@ -79,7 +79,41 @@ async def post_flowentry(post_data):
             "out_port": "ANY",
             "out_group": "ANY",
             "flags": ["SEND_FLOW_REM", "CHECK_OVERLAP" ],
-            "match": {{"field": k.upper(), "value": v} for k,v in post_data['match']},
+            "match": {{"field": k.upper(), "value": v} for k,v in post_data['match'].items()},
+            "instructions": [
+                {
+                    "instruction": "APPLY_ACTIONS",
+                    "actions": actions
+                }
+            ]
+        }
+    }).request(datapath_id=_parse_dpid(dpid))
+    
+    return {dpid: result['msg']}
+
+@WEB.post('/stats/flowentry/modify', 'json')
+async def post_flowentry_modify(post_data):
+    dpid = post_data['dpid']
+    actions = []
+    for v in post_data['actions']:
+        actions.append({"action": v['type'], **v})
+        del actions[-1]['type']
+    result = await zof.compile({
+        "type": "FLOW_MOD",
+        "version": "1.3",
+        "msg": {
+            "cookie": 0,
+            "cookie_mask": 0xFFFFFFFFFFFFFFFF,
+            "table_id": 0,
+            "command": "MODIFY",
+            "idle_timeout": 10,
+            "hard_timeout": 30,
+            "priority": post_data['priority'],
+            "buffer_id": "NO_BUFFER",
+            "out_port": "ANY",
+            "out_group": "ANY",
+            "flags": ["SEND_FLOW_REM", "CHECK_OVERLAP" ],
+            "match": {{"field": k.upper(), "value": v} for k,v in post_data['match'].items()},
             "instructions": [
                 {
                     "instruction": "APPLY_ACTIONS",
